61dacc317e7f467da0bb0af8de624da076d0fbc9
hieujojo <conghieuzc112@gmail.com>
2025-03-01 02:57:54 +0700

    header & footer

---
+++ src/app/home/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/layout/Footer";
+import Header from "@/layout/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
