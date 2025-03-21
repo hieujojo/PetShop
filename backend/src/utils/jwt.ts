84d231350a1c609c1927e203750fcb60dd0b2b26
hieujojo <conghieuzc112@gmail.com>
2025-03-21 22:58:12 +0700

    Fix .gitignore issues

--- backend/src/utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const SECRET_KEY = process.env.JWT_SECRET || "your_secret_key";
-
-export function generateToken(payload: object) {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn: "7d" });
-}
